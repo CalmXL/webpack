96f444da0554da2db48fbf78a73643c44ea6a3cc
xulei <xl18845898715@gmail.com>
2024-11-18 17:57:27 +0800

    feat: devtool 常用配置

--- 06_source-map/build/bundle.js
+++ 06_source-map/build/bundle.js
@@ -1,2 +1,12 @@
-(()=>{"use strict";count,console.log("Hello, World"),console.log(address),console.log("foo function exec...")})();
-//# sourceMappingURL=bundle.js.map
+(() => {
+  'use strict';
+  var __webpack_modules__ = {
+      406: () => {
+        eval(
+          "\n;// ./src/utils/math.js\nfunction sum(a, b) {\r\n  return a + b;\r\n}\r\n\r\ncount;\r\n\r\n\r\n\n;// ./src/main.js\n\r\n\r\nconst message = 'Hello, World';\r\nconsole.log(message);\r\n\r\nconsole.log(address);\r\n\r\nconst foo = () => {\r\n  console.log('foo function exec...');\r\n};\r\n\r\nfoo();\r\n\r\nsum(1, 2);\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNDA2LmpzIiwibWFwcGluZ3MiOiI7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ2U7OztBQ05vQjtBQUNuQztBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxHQUFHIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc291cmNlLW1hcC8uL3NyYy91dGlscy9tYXRoLmpzP2VkYzAiLCJ3ZWJwYWNrOi8vc291cmNlLW1hcC8uL3NyYy9tYWluLmpzPzU2ZDciXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gc3VtKGEsIGIpIHtcclxuICByZXR1cm4gYSArIGI7XHJcbn1cclxuXHJcbmNvdW50O1xyXG5cclxuZXhwb3J0IHsgc3VtIH07XHJcbiIsImltcG9ydCB7IHN1bSB9IGZyb20gJy4vdXRpbHMvbWF0aCc7XHJcblxyXG5jb25zdCBtZXNzYWdlID0gJ0hlbGxvLCBXb3JsZCc7XHJcbmNvbnNvbGUubG9nKG1lc3NhZ2UpO1xyXG5cclxuY29uc29sZS5sb2coYWRkcmVzcyk7XHJcblxyXG5jb25zdCBmb28gPSAoKSA9PiB7XHJcbiAgY29uc29sZS5sb2coJ2ZvbyBmdW5jdGlvbiBleGVjLi4uJyk7XHJcbn07XHJcblxyXG5mb28oKTtcclxuXHJcbnN1bSgxLCAyKTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///406\n"
+        );
+      },
+    },
+    __webpack_exports__ = {};
+  __webpack_modules__[406]();
+})();
--- 06_source-map/webpack.config.js
+++ 06_source-map/webpack.config.js
@@ -1,11 +1,28 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: 'eval-source-map',
+  /**
+   * 不生成source-map
+   * false
+   * none => production
+   * eval => development
+   *
+   * 生成 source-map
+   * source-map => production
+   *
+   * 不常见的值
+   * eval-source-map: 添加到 eval 后面
+   * inline-source-map: 添加到文件的后面
+   * cheap-source-map: 低开销，更加高效
+   *
+   */
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './build'),
     filename: 'bundle.js',
   },
+  plugins: [new CleanWebpackPlugin()],
 };
